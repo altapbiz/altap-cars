418f8c4b70950dfac1429a478b4d3412d2c54d52
altapbiz <xebercix@gmail.com>
2026-08-17 13:51:59 +0400

    server.js

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.json({
-    status: "ok",
-    message: "Altap Cars server is working!"
-  });
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Altap Cars server running on port ${PORT}`);
 });
